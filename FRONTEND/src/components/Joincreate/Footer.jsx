5940ff1f99fdde1e0344a5e2e033e877f9d39fcf
apurbamaity <maityapurba020@gmail.com>
2021-12-06 08:36:31 +0530

    created the join or create a workspace frontend

---
+++ FRONTEND/src/components/Joincreate/Footer.jsx
@@ -0,0 +1,42 @@
+import react from 'react';
+import './Joincreate.css'
+
+
+const Footer = () =>{
+
+
+
+
+        return (
+                <>
+
+
+                <footer style={{height:"200px" , marginTop:"300px"}}>
+
+                <div class="container p-3">
+                        <div class="row">
+                                <div class="col-sm-6 col-lg-6 col-md-6 col-12">
+                                        <p class="h2"> <a class="navbar-brand" href="/"><span>DOCK</span><span>KEEP</span><span>.com</span></a>
+                                        </p>
+                                        <p class="h4 text-white">Keeps your docs at one place.</p>
+                                </div>
+
+                                <div class="col-sm-6 col-lg-6 col-md-6 col-12">
+                                        <p class="p-large text-white">Write for us at dockkeep@gmail.com.</p>
+                                </div>
+
+                        </div>
+                </div>
+
+
+        </footer>
+
+
+                </>
+        )
+
+
+
+}
+
+export default Footer
